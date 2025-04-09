e052e5b4f8b21da95c622029abd94ca81bf677eb
Jayashree Kundu <jayashree2003bbs@gmail.com>
2025-04-09 11:14:41 +0530

    Update Code

--- Micropython and PIO/test_code/blink_led.py
+++ Micropython and PIO/test_code/blink_led.py
@@ -1,8 +1,8 @@
-from machine import Pin
-from time import sleep
+from machine import Pin      # Import Pin class to control GPIO pins
+from time import sleep       # Import sleep to add delay
 
-led=Pin(25,Pin.OUT)
+led = Pin(25, Pin.OUT)       # Initialize GPIO pin 25 as output (onboard LED on Pico)
 
-while True:
-    led.toggle()
-    sleep(0.5)
+while True:                  # Infinite loop
+    led.toggle()             # Toggle the LED state (ON to OFF or OFF to ON)
+    sleep(0.5)               # Wait for 0.5 seconds before toggling again
